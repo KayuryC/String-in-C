e36339e763e4782f0b4c710267cb21283ed62340
Kayruy Carneiro <kayocarneiro77@gmail.com>
2024-05-23 20:47:40 -0300

    Função toupper
    Transforma a String minúscula para maiúscula

---
+++ Basico2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+//Função toupper
+void stringM() {
+	//Declarando variaveis
+	int tamanho;
+	char string[30];
+
+	//Le string
+	scanf("%[^\n]", string);
+	
+	//Processo
+	tamanho= strlen(string);
+	for(int i=0; i < tamanho; i++) {
+		string[i] = toupper(string[i]);
+	}
+	//Saida
+	printf("%s", string);
+	getch();
+	system("cls");
+}
+int main() {
+	while(1) {
+		printf("Digite uma frase ou uma palavra: ");
+		stringM();
+	}
+}
