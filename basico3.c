8ed5c5650ebf09480d3e0cb902991aac2e0fea99
Kayruy Carneiro <kayocarneiro77@gmail.com>
2024-05-31 21:02:53 -0300

    string

---
+++ basico3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+//Função
+void string_invert() {
+	//Declarando variaveis
+	int tamanho;
+	char string[130];
+	char string2[130];
+	int i,j = 0;
+
+	//Le string
+	scanf("%[^\n]", string);
+
+	//Processo
+	tamanho= strlen(string);
+	for(i = tamanho - 1; i >= 0; i--) {
+		string2[j] = string[i];
+		j++;
+	}
+	printf("%s", string2);
+	//Saida
+	getch();
+	system("cls");
+}
+int main() {
+	while(1){	
+	printf("Digite uma frase ou uma palavra: ");
+	string_invert();
+	}
+}
---
+++ basico4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+//Função
+void string_comp() {
+	//Declarando variaveis
+	int tam;
+	char string[130];
+	char string2[130];
+	int i,j = 0;
+
+	//Le string
+	scanf("%[^\n]", string);
+	
+	//Processo
+	for(i = 0; i < tam; i++) {
+		string[i] = toupper(string[i]);
+	}
+	tam = strlen(string);
+	for(i = tam - 1; i >= 0; i--) {
+		string2[j] = string[i];
+		j++;
+	}
+	//Saida
+	if(strcmp(string, string2)== 0) {
+		printf("E um polindromo");
+	} else {
+		printf("Nao e um polindromo");
+	}
+	
+}
+int main() {	
+		printf("Digite uma frase ou uma palavra: ");
+		string_comp();
+}
